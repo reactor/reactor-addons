71154012a3e1c0d8cc28368823cd8022baad633f
Alex Petrov <oleksandr.petrov@gmail.com>
2016-03-01 21:18:24 +0100

    Add missing import

--- reactor-pipe/src/test/java/reactor/pipe/AbstractPipeTests.java
+++ reactor-pipe/src/test/java/reactor/pipe/AbstractPipeTests.java
@@ -7,6 +7,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
 
 import org.junit.Test;
 import org.pcollections.TreePVector;
