00d1055bc86dcc1fe9a6823c0521b0d43ea0b355
Oleh Dokuka <shadowgun@i.ua>
2017-11-14 00:35:57 +0200

    #130 provide implementation of the simple cache helper
    The current implementation is based on in-memory Cache such as Caffeine.
    In this case, the in-memory cache must be presented as Key-Value Map.

--- reactor-extra/src/main/java/reactor/cache/Cache.java
+++ reactor-extra/src/main/java/reactor/cache/Cache.java
@@ -1,69 +1,115 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package reactor.cache;
 
 import java.util.Map;
-import java.util.function.Function;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
+import reactor.util.annotation.NonNull;
 
-@SuppressWarnings("unchecked")
+/**
+ * Cache helper that may be used with any cache vendors that has a Map wrapper support.
+ * <p>
+ * Example usage:
+ * <pre><code>
+ *    LoadingCache<Integer, Object> graphs = Caffeine.newBuilder()
+ *                                       .maximumSize(10_000)
+ *                                       .expireAfterWrite(5, TimeUnit.MINUTES)
+ *                                       .refreshAfterWrite(1, TimeUnit.MINUTES)
+ *                                       .build(key -> createExpensiveGraph(key));
+ *
+ *    keyStream.concatMap(key -> Cache.hydrateFrom(graphs.asMap(), key)
+ *                                   .or(repository.findOneById(key))
+ * </code></pre>
+ *
+ * @author Oleh Dokuka
+ */
+
 public class Cache {
 
-	public static <T> FluxCache.KeyExtractorRequired<T> from(Flux<T> upstream) {
-		return new FluxCacheBuilder(upstream);
+	private static final Object NULL_HOLDER = new Object();
+
+	/**
+	 * Current method may be used to drain the value from the cache-map by given key and
+	 * transform the result to {@link Mono<Value>}. In the case of empty value the value
+	 * will be calculated from the alternative source. Note, if the alternative source
+	 * returns empty result, it will be consider as the result and for all subsequent
+	 * requests with the identical key will be returned {@link Mono#empty()). The similar
+	 * behaviours is for the case when alternative source completed with error, thus for
+	 * all subsequent identical keys will be emitted {@link Mono#error(Throwable)}
+	 *
+	 * @param cache {@link Map} wrapper of a cache
+	 * @param key mapped key
+	 * @param <Key> Key Type
+	 * @param <Value> Value Type
+	 *
+	 * @return Lazy "Or" Builder
+	 */
+	@NonNull
+	public static <Key, Value> Or<Value> hydrateFrom(@NonNull Map<Key, Value> cache,
+			@NonNull Key key) {
+		return other -> {
+			Value cached = cache.get(key);
+
+			if (cached == null) {
+				return other.materialize()
+				            .doOnNext(value -> cache.put(key, get(value)))
+				            .dematerialize();
+			}
+			else if (cached == NULL_HOLDER) {
+				return Mono.empty();
+			}
+			else if (cached instanceof ErrorHolder) {
+				return Mono.error(((ErrorHolder) cached).e);
+			}
+			else {
+				return Mono.just(cached);
+			}
+		};
 	}
 
-	public static <T> MonoCache.KeyExtractorRequired<T> from(Mono<T> upstream) {
-		return new MonoCacheBuilder(upstream);
+	@SuppressWarnings("unchecked")
+	private static <Value> Value get(Signal<? extends Value> signal) {
+		Value value = signal.get();
+
+		return signal.hasError() ? (Value) new ErrorHolder(signal.getThrowable()) :
+				value == null ? (Value) NULL_HOLDER : value;
 	}
 
-	public static <T> CacheRequired<T> cache(Mono<? extends T> mono) {
-		return new SimpleCacheBuilder(mono);
+	public interface Or<Value> {
+
+		/**
+		 * Fallback {@link Mono<Value>} in case of missing value in cache
+		 *
+		 * @param other fallback
+		 *
+		 * @return Mono with value
+		 */
+		@NonNull
+		Mono<Value> or(Mono<? extends Value> other);
 	}
 
-	public interface CacheRequired<Out> {
+	private static class ErrorHolder {
 
-		<Key> KeyRequired<Key, Out> in(Map<? super Key, ? super Out> cacheMap);
-	}
+		private final Throwable e;
 
-	public interface KeyRequired<Key, Out> {
-
-		Mono<Out> by(Key key);
-	}
-
-	public interface MonoCache {
-
-		interface KeyExtractorRequired<In> extends CacheRequired<In> {
-
-			<Key> CacheRequired<Key> by(Function<? super In, ? extends Key> extractor);
-		}
-
-		interface CacheRequired<Key> {
-
-			<Out> DownstreamRequired<Key, Out> in(Map<? super Key, ? super Out> cacheMap);
-		}
-
-		interface DownstreamRequired<Key, Out> {
-
-			Mono<Out> computeIfEmpty(Function<? super Key, Mono<? extends Out>> downstreamFunction);
-		}
-	}
-
-	public interface FluxCache {
-
-		interface KeyExtractorRequired<In> extends CacheRequired<In> {
-
-			<Key> CacheRequired<Key> by(Function<? super In, ? extends Key> extractor);
-		}
-
-		interface CacheRequired<Key> {
-
-			<Out> DownstreamRequired<Key, Out> in(Map<? super Key, ? super Out> cacheMap);
-		}
-
-		interface DownstreamRequired<Key, Out> {
-
-			Flux<Out> computeIfEmpty(Function<? super Key, Mono<? extends Out>> downstreamFunction);
+		private ErrorHolder(Throwable e) {
+			this.e = e;
 		}
 	}
 }
--- reactor-extra/src/test/java/reactor/cache/ReactorCacheTest.java
+++ reactor-extra/src/test/java/reactor/cache/ReactorCacheTest.java
@@ -1,210 +1,119 @@
 package reactor.cache;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.junit.Assert;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
 import reactor.test.StepVerifier;
 
 public class ReactorCacheTest {
 
 	@Test
-	public void simpleFluxCache() {
-		Flux<Integer> source = Flux.just(4567, 6789);
-		Map<String, List<Integer>> cacheStore = new HashMap<>();
-		StepVerifier.withVirtualTime(() -> Cache.cache(source.delaySubscription(Duration.ofMillis(
-				1000))
-		                                                     .collectList())
-		                                        .in(cacheStore)
-		                                        .by("test"))
+	public void shouldCacheValue() {
+		Flux<Integer> source = Flux.just(1, 2, 3, 4);
+		Map<Integer, String> cache = new HashMap<>();
+		Function<Integer, Mono<String>> flatMap = key -> Cache.hydrateFrom(cache, key)
+		                                                      .or(Mono.just(key)
+		                                                              .transform(delay()));
+
+		StepVerifier.withVirtualTime(() -> source.concatMap(flatMap))
 		            .expectSubscription()
 		            .expectNoEvent(Duration.ofMillis(1000))
-		            .expectNext(Arrays.asList(4567, 6789))
-		            .expectComplete()
-		            .verify();
-
-		Assert.assertTrue(cacheStore.containsKey("test"));
-		Assert.assertEquals(1, cacheStore.size());
-	}
-
-	@Test
-	public void fluxCache() {
-		Flux<Integer> upstream = Flux.just(4567);
-		Map<String, List<String>> cacheStore = new HashMap<>();
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .by(String::valueOf)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Flux.fromArray(key.split(
-				                                        ""))
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))
-		                                                                   .collectList())
-		                                        .flatMapIterable(i -> i))
-		            .expectSubscription()
-		            .expectNoEvent(Duration.ofMillis(1000))
-		            .expectNext("4", "5", "6", "7")
-		            .expectComplete()
-		            .verify();
-
-		Assert.assertTrue(cacheStore.containsKey("4567"));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertArrayEquals(Arrays.asList("4", "5", "6", "7")
-		                               .toArray(),
-				cacheStore.get("4567")
-				          .toArray());
-	}
-
-	@Test
-	public void fluxCacheWithNoExtractor() {
-		Flux<Integer> upstream = Flux.just(4567);
-		Map<Integer, List<String>> cacheStore = new HashMap<>();
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Flux.fromArray(key.toString()
-		                                                                                 .split(""))
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))
-		                                                                   .collectList())
-		                                        .flatMapIterable(i -> i))
-		            .expectSubscription()
-		            .expectNoEvent(Duration.ofMillis(1000))
-		            .expectNext("4", "5", "6", "7")
-		            .expectComplete()
-		            .verify();
-
-		Assert.assertTrue(cacheStore.containsKey(4567));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertArrayEquals(Arrays.asList("4", "5", "6", "7")
-		                               .toArray(),
-				cacheStore.get(4567)
-				          .toArray());
-	}
-
-	@Test
-	public void monoCache() {
-		Mono<Integer> upstream = Mono.just(4567);
-		Map<String, String> cacheStore = new HashMap<>();
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .by(String::valueOf)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Mono.just(key)
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))))
-		            .expectSubscription()
-		            .expectNoEvent(Duration.ofMillis(1000))
-		            .expectNext("4567")
-		            .expectComplete()
-		            .verify();
-
-		Assert.assertTrue(cacheStore.containsKey("4567"));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertEquals("4567", cacheStore.get("4567"));
-	}
-
-	@Test
-	public void monoCacheWithNoExtractor() {
-		Mono<Integer> upstream = Mono.just(4567);
-		Map<Integer, String> cacheStore = new HashMap<>();
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Mono.just(key.toString())
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))))
-		            .expectSubscription()
-		            .expectNoEvent(Duration.ofMillis(1000))
-		            .expectNext("4567")
-		            .expectComplete()
-		            .verify();
-
-		Assert.assertTrue(cacheStore.containsKey(4567));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertEquals("4567", cacheStore.get(4567));
-	}
-
-	@Test
-	public void fluxFromCache() {
-		Flux<Integer> upstream = Flux.just(4567);
-		Map<String, List<String>> cacheStore = new HashMap<>();
-		cacheStore.put("4567", Arrays.asList("4", "5", "6", "7"));
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .by(String::valueOf)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Flux.fromArray(key.split(
-				                                        ""))
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))
-		                                                                   .collectList())
-		                                        .flatMapIterable(i -> i))
-		            .expectSubscription()
-		            .expectNext("4", "5", "6", "7")
-		            .expectComplete()
-		            .verify(Duration.ofMillis(100));
-
-		Assert.assertTrue(cacheStore.containsKey("4567"));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertArrayEquals(Arrays.asList("4", "5", "6", "7")
-		                               .toArray(),
-				cacheStore.get("4567")
-				          .toArray());
-	}
-
-	@Test
-	public void monoFromCache() {
-		Mono<Integer> upstream = Mono.just(4567);
-		Map<String, String> cacheStore = new HashMap<>();
-		cacheStore.put("4567", "4567");
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .by(String::valueOf)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Mono.just(key)
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))))
-		            .expectSubscription()
-		            .expectNext("4567")
-		            .expectComplete()
-		            .verify(Duration.ofMillis(100));
-
-		Assert.assertTrue(cacheStore.containsKey("4567"));
-		Assert.assertEquals(1, cacheStore.size());
-		Assert.assertEquals("4567", cacheStore.get("4567"));
-	}
-
-	@Test
-	public void severalValuesFlux() {
-		Flux<Integer> upstream = Flux.just(4567, 78910, 111213);
-		Map<String, String> cacheStore = new HashMap<>();
-		cacheStore.put("4567", "4567");
-		StepVerifier.withVirtualTime(() -> Cache.from(upstream)
-		                                        .by(String::valueOf)
-		                                        .in(cacheStore)
-		                                        .computeIfEmpty(key -> Mono.just(key)
-		                                                                   .delaySubscription(
-				                                                                   Duration.ofMillis(
-						                                                                   1000))))
-		            .expectSubscription()
-		            .expectNext("4567")
+		            .expectNext("1")
 		            .thenAwait(Duration.ofMillis(1000))
-		            .expectNext("78910")
+		            .expectNext("2")
 		            .thenAwait(Duration.ofMillis(1000))
-		            .expectNext("111213")
+		            .expectNext("3")
+		            .thenAwait(Duration.ofMillis(1000))
+		            .expectNext("4")
 		            .expectComplete()
 		            .verify();
 
-		Assert.assertEquals(3, cacheStore.size());
-		Assert.assertEquals("4567", cacheStore.get("4567"));
-		Assert.assertEquals("78910", cacheStore.get("78910"));
-		Assert.assertEquals("111213", cacheStore.get("111213"));
+		Assert.assertEquals(4, cache.size());
+	}
+
+	@Test
+	public void shouldRestoreValueFromCache() {
+		Flux<Integer> source = Flux.just(1, 3);
+
+		Map<Integer, String> cache = new HashMap<>();
+		cache.put(1, "2");
+
+		Function<Integer, Mono<String>> flatMap = key -> Cache.hydrateFrom(cache, key)
+		                                                      .or(Mono.just(key)
+		                                                              .transform(delay()));
+
+		StepVerifier.withVirtualTime(() -> source.concatMap(flatMap))
+		            .expectSubscription()
+		            .expectNext("2")
+		            .expectNoEvent(Duration.ofMillis(1000))
+		            .expectNext("3")
+		            .expectComplete()
+		            .verify();
+
+		Assert.assertEquals(2, cache.size());
+	}
+
+	@Test
+	public void shouldCacheNullHolder() {
+		Flux<Integer> source = Flux.just(1, 1, 1, 1);
+		Map<Integer, String> cache = new HashMap<>();
+
+		Function<Integer, Mono<String>> flatMap = key -> Cache.hydrateFrom(cache, key)
+		                                                      .or(Mono.just("")
+		                                                              .transform(delay())
+		                                                              .flatMap(v -> Mono.empty()));
+
+		StepVerifier.withVirtualTime(() -> source.concatMap(flatMap)
+		                                         .materialize()
+		                                         .repeat(4))
+		            .expectSubscription()
+		            .expectNoEvent(Duration.ofMillis(1000))
+		            .expectNext(Signal.complete())
+		            .expectNext(Signal.complete())
+		            .expectNext(Signal.complete())
+		            .expectNext(Signal.complete())
+		            .expectComplete()
+		            .verify();
+
+		Assert.assertEquals(1, cache.size());
+	}
+
+	@Test
+	public void shouldCacheError() {
+		Flux<Integer> source = Flux.just(1);
+		Map<Integer, String> cache = new HashMap<>();
+		NullPointerException npe = new NullPointerException();
+
+		Function<Integer, Mono<String>> flatMap = key -> Cache.hydrateFrom(cache, key)
+		                                                      .or(Mono.just("")
+		                                                              .transform(delay())
+		                                                              .flatMap(v -> Mono.error(
+				                                                              npe)));
+
+		StepVerifier.withVirtualTime(() -> source.concatMap(flatMap)
+		                                         .materialize()
+		                                         .repeat(4))
+		            .expectSubscription()
+		            .expectNoEvent(Duration.ofMillis(1000))
+		            .expectNext(Signal.error(npe))
+		            .expectNext(Signal.error(npe))
+		            .expectNext(Signal.error(npe))
+		            .expectNext(Signal.error(npe))
+		            .expectComplete()
+		            .verify();
+
+		Assert.assertEquals(1, cache.size());
+	}
+
+	private static Function<Mono<?>, Mono<String>> delay() {
+		return in -> in.map(String::valueOf)
+		               .delayElement(Duration.ofMillis(1000));
 	}
 }
