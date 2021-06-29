0b149b739fe680bd78306c16fa4bf46a799c0e1c
Simon Baslé <sbasle@vmware.com>
2021-06-29 17:22:14 +0200

    Fixing a few corner cases (single year pivotal, the original author...)

--- reactor-extra/src/main/java/reactor/function/TupleUtils.java
+++ reactor-extra/src/main/java/reactor/function/TupleUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2017 the original author or authors.
+ * Copyright (c) 2016-2021 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
