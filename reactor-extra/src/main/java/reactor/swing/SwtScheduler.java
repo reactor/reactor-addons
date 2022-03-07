d8de2c1a52f26e716799a14cadf4327422ec0c9e
Simon Baslé <sbasle@vmware.com>
2022-03-07 17:50:49 +0100

    polish wording of deprecation notice

--- reactor-extra/src/main/java/reactor/swing/SwtScheduler.java
+++ reactor-extra/src/main/java/reactor/swing/SwtScheduler.java
@@ -29,7 +29,7 @@ import reactor.util.context.Context;
 
 /** 
  * Scheduler that runs tasks on Swt's event dispatch thread. 
- * @deprecated To be removed exceptionally fast in 3.5.0. See https://github.com/reactor/reactor-addons/issues/273
+ * @deprecated To be removed in 3.5.0 (fast tracked). See https://github.com/reactor/reactor-addons/issues/273
  */
 @Deprecated
 public final class SwtScheduler implements Scheduler {
--- reactor-extra/src/test/java/reactor/swing/SwingAdapterTest.java
+++ reactor-extra/src/test/java/reactor/swing/SwingAdapterTest.java
@@ -25,7 +25,7 @@ import reactor.test.StepVerifier;
 
 /**
  * @author Stephane Maldini
- * @deprecated To be removed exceptionally fast in 3.5.0. See https://github.com/reactor/reactor-addons/issues/273
+ * @deprecated To be removed in 3.5.0 (fast tracked). See https://github.com/reactor/reactor-addons/issues/273
  */
 @Deprecated
 public class SwingAdapterTest {
